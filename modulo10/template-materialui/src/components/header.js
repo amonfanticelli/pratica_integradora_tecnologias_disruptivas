f1d447fd5fea9b1a4555108205b8b54763d15013
amon <amon_fanticelli@hotmail.com>
2024-12-02 16:57:40 -0300

    style: background color changed

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -1,35 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
- 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000", // Define preto como a cor principal
+    },
+  },
+});
+
 function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Gerenciamento de Projetos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+  );
 }
- 
+
 export default Header;
